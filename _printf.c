7d173ba033675aff6b3f5509c925c40e8f47a46f
AliiMustafa <aliimustafazada@gmail.com>
2024-07-22 19:03:20 +0400

    Change return value

--- _printf.c
+++ _printf.c
@@ -1,12 +1,16 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stddef.h>
 
+/**
+ * _printf - function
+ * @format: pointer
+ * Return: lengs
+ */
 int _printf(const char *format, ...)
 {
 	va_list arr;
 	int i = 0, j = 0, leng = 0;
 	char *temp;
+
 
 	va_start(arr, format);
 
@@ -14,35 +18,46 @@ int _printf(const char *format, ...)
 	{
 		while(format[i])
 		{
-			if (format[i] == '%' && format[i + 1] == 'c')
+			if (format[0] == '%' && !format[1])
+			{
+				return (-1);
+			}
+			else if (format[i] == '%' && format[i + 1] == 'c')
 			{
 				_putchar(va_arg(arr, int));
 				i = i + 2;
+				leng++;
 			}
 			else if (format[i] == '%' && format[i + 1] == 's')
 			{
 				temp = va_arg(arr, char*);
+				if (temp == NULL)
+				{
+					temp = "(null)";
+				}
 				while (temp[j])
 				{
 					_putchar(temp[j]);
 					j++;
 				}
-				leng = leng + j - 2;
+				leng = leng + j;
 				i = i + 2;
+				j = 0;
 			}
 			else if (format[i] == '%' && format[i + 1] == '%')
 			{
 				_putchar('%');
 				i = i + 2;
+				leng++;
 			}
 			else
 			{
 				_putchar(format[i]);
 				i++;
+				leng++;
 			}
 		}
 	}
 	va_end(arr);
-	leng = leng + i;
 	return (leng);
 }
