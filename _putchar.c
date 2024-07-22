67f5e1a8dd2afc6a7c2697d990909d396325cb52
AliiMustafa <aliimustafazada@gmail.com>
2024-07-22 19:10:17 +0400

    Betty coding style

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
-
-int _putchar(char c) 
+/**
+ * _putchar - function
+ * @c: variable
+ * Return: 0
+ */
+int _putchar(char c)
 {
 	write(1, &c, 1);
-	return 0;
+	return (0);
 }
